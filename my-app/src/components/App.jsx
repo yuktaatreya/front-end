8991ff8eb2b477a3d4c963539c001918b6887d87
unknown <yuktatn@gmail.com>
2021-12-16 15:42:15 +0530

    add map function for notes,fixed copyright footer issue

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import Header  from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes";
+
+notes.map(function(note){
+    return <Note  key={note.id} title={note.title} content={note.content} />
+});
 function App(){
     return <div>
         <Header />
-        <Note />
+        {
+            notes.map(function(note){
+                return <Note  key={note.id} title={note.title} content={note.content} />})
+        }
+        
         <Footer />
     </div>
 }
--- my-app/src/components/Note.jsx
+++ my-app/src/components/Note.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1>Title</h1>
-        <p>Content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 }
 export default Note;
